9ff8635f9896b161c4067ff7579f5fa41a6e510e
Kyra <klz245@nyu.edu>
2020-09-15 10:36:33 -0400

    Added project files
    Unity files have been added for the first time

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (gameObject.transform.position.y < 10)
+            {
+                transform.Translate(0, 0.2f, 0);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (gameObject.transform.position.y > -10)
+            {
+                transform.Translate(0, -0.2f, 0);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (gameObject.transform.position.x < 10)
+            {
+                transform.Translate(0.2f, 0, 0);
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (gameObject.transform.position.x > -10)
+            {
+                transform.Translate(-0.2f, 0, 0);
+            }
+        }
+        
+    }
+}
